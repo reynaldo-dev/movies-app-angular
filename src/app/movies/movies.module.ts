0c073e5cd751a8fc14cdabae5939f03099f4414a
reynaldo-dev <reynaldo23.dev@gmail.com>
2023-05-01 16:14:26 -0600

    [ADD] header design

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    MoviesComponent
-  ],
-  imports: [
-    CommonModule,
-    MoviesRoutingModule
-  ]
+  declarations: [MoviesComponent],
+  imports: [CommonModule, MoviesRoutingModule, SharedModule],
 })
-export class MoviesModule { }
+export class MoviesModule {}
